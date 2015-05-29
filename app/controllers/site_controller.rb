d4e05e69e41e33908236446cba6edf6cc95fcd1d
Aidi Stan <aidistan@live.com>
2015-05-29 23:11:03 +0800

    Implement weixin verification

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -1,4 +1,22 @@
 class SiteController < ApplicationController
+  # Weixin token
+  TOKEN = 'TiZV27gJjde7W78gX7FoP4nvNwBdaPTi'
+
   def home
+    render plain: params['echostr'] if check_signature
+  end
+
+  private
+
+  def check_signature()
+    signature = params['signature'] or return false
+    timestamp = params['timestamp'] or return false
+    nonce = params['nonce'] or return false
+
+    if (signature == Digest::SHA1.hexdigest([TOKEN, timestamp, nonce].sort.join))
+      return true
+    else
+      return false
+    end
   end
 end
